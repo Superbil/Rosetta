63a801952486105ef9093914ef9d242f3d3af069
Superbil <superbil@gmail.com>
2015-09-07 05:29:14 +0800

    Fix bug on print value have b

--- apStringToLocalizable.py
+++ apStringToLocalizable.py
@@ -91,10 +91,8 @@ for l in range(len(language)):
                 else:
                     # null string will been pass
                     if key and value:
-                        v = value.encode('utf-8')
                         if not args.quite:
-                            print(context_line_format.format(key, v))
-                        f.write(context_line_format.format(key, v) + '\n')
-
+                            print(context_line_format.format(key, value))
+                        f.write(context_line_format.format(key, value) + '\n')
     except KeyError:
         pass
